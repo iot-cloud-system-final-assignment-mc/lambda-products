dd85c7218a5df1704ba0d0f67b185f26c2e6549d
Mirkesx <cmarcos46@gmail.com>
2022-09-14 13:27:22 +0200

    init repo: fused both get-products and upsert-product repos, also add deleteProduct function

---
+++ code/index.js
@@ -0,0 +1,70 @@
+const AWS = require('aws-sdk');
+const { buildResponse } = require('/opt/http-builder');
+
+
+module.exports.getProducts = async (event, context) => {
+    try {
+        const dynamoDb = new AWS.DynamoDB.DocumentClient();
+        const params = {
+            TableName: 'Products'
+        };
+        const result = await dynamoDb.scan(params).promise();
+        return buildResponse(200, result["Items"]);
+    } catch (error) {
+        console.log(error);
+        return buildResponse(500, error.message);
+    }
+}
+// END
+
+module.exports.upsertProduct = async (event, context) => {
+    try {
+        const body = JSON.parse(event["body"]);
+        const id = body.product_id || uuidv4();
+        const updated_at = Date.now();
+        const dynamoDb = new AWS.DynamoDB.DocumentClient();
+        const params = {
+            TableName: 'Products',
+            Key: {
+                product_id: id
+            },
+            UpdateExpression: 'set #name = :name, #price = :price, #updated_at = :updated_at',
+            ExpressionAttributeNames: {
+                '#name': 'name',
+                '#price': 'price',
+                '#updated_at': 'updated_at'
+            },
+            ExpressionAttributeValues: {
+                ':name': body.name,
+                ':price': body.price,
+                ':updated_at': updated_at
+            }
+        };
+
+        const result = await dynamoDb.update(params).promise();
+        return buildResponse(200, result);
+    } catch (error) {
+        console.log(error);
+        return buildResponse(500, error.message);
+    };
+}
+// END
+
+module.exports.deleteProduct = async (event, context) => {
+    try {
+        const dynamoDb = new AWS.DynamoDB.DocumentClient();
+        const params = {
+            TableName: 'Products',
+            Key: {
+                product_id: event.pathParameters.product_id
+            },
+            ReturnValues: 'ALL_OLD'
+        };
+        const result = await dynamoDb.delete(params).promise();
+        return buildResponse(200, result);
+    } catch (error) {
+        console.log(error);
+        return buildResponse(500, error.message);
+    }
+}
+    // END
